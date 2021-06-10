49fe487993170fabd110793edb8a4b46dbe06881
Kostya Khushensky <46225922+KonstDev@users.noreply.github.com>
2021-06-10 12:54:22 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from pypresence import Presence
+import time
+
+client_id = "your_cliend_id"
+
+status = 'Github: @KonstDev'
+
+RPC = Presence(client_id)
+RPC.connect()
+
+status1 = status[0] + status[1]
+i = 2
+while(True):
+     RPC.update(state=status1)
+     if (len(status1) == len(status)):
+          status1 = status[0] + status[1]
+          i = 2
+          time.sleep(1)
+     else:
+          status1 += status[i]
+          i += 1
+          time.sleep(0.15)
